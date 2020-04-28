f4025a7ea8fb3b17072b7311c68fff4b3b909d6a
Mustapha Ben Chaaben <mustapha3892@gmail.com>
2020-04-28 20:45:17 +0200

    Fixed typos and run codedoc b

--- docs/assets/codedoc-bundle.entry.js
+++ docs/assets/codedoc-bundle.entry.js
@@ -1,31 +1,31 @@
-import { getRenderer } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
-import { initJssCs } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
-import { installTheme } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/content/theme.ts';installTheme();
-import { codeSelection } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
-import { sameLineLengthInCodes } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
-import { initHintBox } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
-import { initCodeLineRef } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
-import { initSmartCopy } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
-import { copyHeadings } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
-import { contentNavHighlight } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
-import { loadDeferredIFrames } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
-import { smoothLoading } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
-import { tocHighlight } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
-import { postNavSearch } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
-import { ToCPrevNext } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
-import { ToCToggle } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
-import { DarkModeSwitch } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
-import { ConfigTransport } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
-import { JoinCreatorButton } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/components/join-creator/button.tsx';
-import { JoinButton } from '/Users/eugeneghanizadeh/Documents/projects/connect/coding-blog/.codedoc/components/join-beta/button.tsx';
+import { getRenderer } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
+import { initJssCs } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
+import { installTheme } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/content/theme.ts';installTheme();
+import { codeSelection } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
+import { sameLineLengthInCodes } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
+import { initHintBox } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
+import { initCodeLineRef } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
+import { initSmartCopy } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
+import { copyHeadings } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
+import { contentNavHighlight } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
+import { loadDeferredIFrames } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
+import { smoothLoading } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
+import { tocHighlight } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
+import { postNavSearch } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
+import { ToCPrevNext } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
+import { ToCToggle } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
+import { DarkModeSwitch } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
+import { ConfigTransport } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
+import { JoinCreatorButton } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/components/join-creator/button.tsx';
+import { JoinButton } from '/Users/mustaphabenchaaben/Code/codedoc/coding-blog/.codedoc/components/join-beta/button.tsx';
 
 const components = {
-  'B4qGPIpQsDNCJH8FY6tycg==': ToCPrevNext,
-  'rPglIEEYZYKWUnckwk5vUQ==': ToCToggle,
-  'Cu+d3UWVY04iUTKlEwuYjQ==': DarkModeSwitch,
-  '6bXLvEFrI86RM1mrMs4nDQ==': ConfigTransport,
-  'Q3h3LfieWiBtoqwf+s4nGw==': JoinCreatorButton,
-  '9CkyEtMypIrJ2qJioHYW1w==': JoinButton
+  'LwMmQh4ocAvMl3KEQwIwbA==': ToCPrevNext,
+  'Y1n70jAnr09ZOarvFk4iaQ==': ToCToggle,
+  '9YlkltvP6FYr6bYgtAh4Ew==': DarkModeSwitch,
+  'oaFrqfOoxxRMwluwPaYCzw==': ConfigTransport,
+  'mDpGZd+WvWCgVvTcZCyvEg==': JoinCreatorButton,
+  '3bgFieq9H5R/a1mQgtxfLA==': JoinButton
 };
 
 const renderer = getRenderer();
