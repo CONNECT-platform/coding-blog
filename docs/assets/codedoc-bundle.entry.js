520b53312cd0e2bf0d12b5e30da53461a718ddf5
loreanvictor <ghanizadeh.eugene@gmail.com>
2020-05-10 20:51:34 +0000

    Deploying to gh-pages from  @ 48bcf9d7e5b77a90ec87612c4c4263a2a023b844 🚀

--- docs/assets/codedoc-bundle.entry.js
+++ docs/assets/codedoc-bundle.entry.js
@@ -13,20 +13,42 @@ import { smoothLoading } from '/home/runner/work/coding-blog/coding-blog/.codedo
 import { tocHighlight } from '/home/runner/work/coding-blog/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
 import { postNavSearch } from '/home/runner/work/coding-blog/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
 import { zoomOnFormula } from '/home/runner/work/coding-blog/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/formula/zoom-on-formula.js';zoomOnFormula();
-import { ToCPrevNext } from '/home/runner/work/coding-blog/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
+import { CollapseControl } from '/home/runner/work/coding-blog/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/collapse/collapse-control.js';
 import { ToCToggle } from '/home/runner/work/coding-blog/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
 import { DarkModeSwitch } from '/home/runner/work/coding-blog/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
 import { ConfigTransport } from '/home/runner/work/coding-blog/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
+import { ToCPrevNext } from '/home/runner/work/coding-blog/coding-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
 import { JoinCreatorButton } from '/home/runner/work/coding-blog/coding-blog/.codedoc/components/join-creator/button.tsx';
 import { JoinButton } from '/home/runner/work/coding-blog/coding-blog/.codedoc/components/join-beta/button.tsx';
+import { ACSBControl } from '/home/runner/work/coding-blog/coding-blog/.codedoc/components/account/status-bound/control.tsx';
+import { AccountProp } from '/home/runner/work/coding-blog/coding-blog/.codedoc/components/account/account-prop/inline.tsx';
+import { AccountPropCode } from '/home/runner/work/coding-blog/coding-blog/.codedoc/components/account/account-prop/code.tsx';
+import { EditNameButton } from '/home/runner/work/coding-blog/coding-blog/.codedoc/components/account/edit-name/button.tsx';
+import { LogoutButton } from '/home/runner/work/coding-blog/coding-blog/.codedoc/components/account/logout/index.tsx';
+import { LoginButton } from '/home/runner/work/coding-blog/coding-blog/.codedoc/components/account/login/button.tsx';
+import { ACPBControl } from '/home/runner/work/coding-blog/coding-blog/.codedoc/components/account/prop-bound/control.tsx';
+import { BindRepoButton } from '/home/runner/work/coding-blog/coding-blog/.codedoc/components/account/bind-repo/button.tsx';
+import { RefreshPublishWebhookButton } from '/home/runner/work/coding-blog/coding-blog/.codedoc/components/account/publish-blog/refresh-button.tsx';
+import { PublishButton } from '/home/runner/work/coding-blog/coding-blog/.codedoc/components/account/publish-blog/button.tsx';
 
 const components = {
-  'dPH0+SmZ7R38b/oBW5P5Rw==': ToCPrevNext,
+  'r+Jv/VS/YauITdU0M0jN1Q==': CollapseControl,
   'krgrG9oFN9VQUm9kMirw5Q==': ToCToggle,
   'rt07Ne+r5k791oQZALhn6g==': DarkModeSwitch,
   '5C7mlfiypfDBeBwm8QT42g==': ConfigTransport,
+  'dPH0+SmZ7R38b/oBW5P5Rw==': ToCPrevNext,
   'lMESGbHzhfX6M8dUvLqFOQ==': JoinCreatorButton,
-  'vnuJLwoGViEp/Odb1ab7Ow==': JoinButton
+  'vnuJLwoGViEp/Odb1ab7Ow==': JoinButton,
+  'rAwarKynpy/O96Cwe2ABsw==': ACSBControl,
+  '1Jf+OzRlMLYJPPZCWaMIDw==': AccountProp,
+  'PR4C5z8XCIWqC0wh4L8big==': AccountPropCode,
+  'shWTIDfMQXEiFAVtcMSW6Q==': EditNameButton,
+  '2iDZbWXSp9mVHyg4ZHFpxg==': LogoutButton,
+  '2UJH07mrL6zqL5jrlN00iA==': LoginButton,
+  'iuBoj8gqUJWoao5Rhv6Z5A==': ACPBControl,
+  'kWYPxjjS6eHniJ5+rq7lbQ==': BindRepoButton,
+  'pT/+mkb6LyaiJVrBv/2nQg==': RefreshPublishWebhookButton,
+  'zJTkEHU8i5aA8SWu2KQczA==': PublishButton
 };
 
 const renderer = getRenderer();
