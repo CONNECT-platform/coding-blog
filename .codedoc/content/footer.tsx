ffb9988ef896e341b841244fed08594495456f9a
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-06-07 16:10:24 +0200

    add social links

--- .codedoc/content/footer.tsx
+++ .codedoc/content/footer.tsx
@@ -1,19 +1,70 @@
-import { CodedocConfig } from '@codedoc/core';
-import { Footer as _Footer, GitterToggle$, Watermark} from '@codedoc/core/components';
+import { CodedocConfig, CodedocTheme } from '@codedoc/core';
+import { Footer as _Footer } from '@codedoc/core/components';
+import { themedStyle, ThemedComponentThis } from '@connectv/jss-theme';
 
 
-export function Footer(config: CodedocConfig, renderer: any) {
-  let github$;
-  if (config.misc?.github)
-    github$ = <a href={`https://github.com/${config.misc.github.user}/${config.misc.github.repo}/`} 
-                target="_blank">GitHub</a>;
+const style = themedStyle<CodedocTheme>(() => ({
+  imgLink: {
+    position: 'relative',
+    width: 24,
+    height: 24,
+    opacity: .25,
+    transition: 'opacity .1s !important',
+    '&:hover': { opacity: 1, },
 
-  let community$;
-  if (config.misc?.gitter)
-    community$ = <GitterToggle$ room={config.misc.gitter.room}/>
+    '& img': {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: 0,
+      left: 0,
+      transition: 'opacity .3s',
 
-  if (github$ && community$) return <_Footer>{github$}<hr/>{community$}</_Footer>;
-  else if (github$) return <_Footer>{github$}</_Footer>;
-  else if (community$) return <_Footer>{community$}</_Footer>;
-  else return <_Footer><Watermark/></_Footer>;
+      '&.light': {
+        'body.dark &': {
+          opacity: 0,
+        },
+    
+        '@media (prefers-color-scheme: dark)': {
+          'body:not(.dark-mode-animate) &': {
+            opacity: 0
+          }
+        },
+      },
+
+      '&.dark': {
+        opacity: 0,
+        'body.dark &': {
+          opacity: 1,
+        },
+    
+        '@media (prefers-color-scheme: dark)': {
+          'body:not(.dark-mode-animate) &': {
+            opacity: 1
+          }
+        },
+      },
+    }
+  }
+}));
+
+
+export function Footer(this: ThemedComponentThis, config: CodedocConfig, renderer: any) {
+  const classes = this.theme.classes(style);
+  return <_Footer>
+    <a href="https://twitter.com/coding_blog" target="_blank" class={classes.imgLink}>
+      <img src="/img/social/twitter-light.svg" class="light"/>
+      <img src="/img/social/twitter-dark.svg" class="dark"/>
+    </a>
+    <hr/>
+    <a href="https://github.com/CONNECT-platform/coding-blog" target="_blank" class={classes.imgLink}>
+      <img src="/img/social/github-light.svg" class="light"/>
+      <img src="/img/social/github-dark.svg" class="dark"/>
+    </a>
+    <hr/>
+    <a href="https://www.linkedin.com/company/coding-blog/" target="_blank" class={classes.imgLink}>
+    <img src="/img/social/linkedin-light.svg" class="light"/>
+      <img src="/img/social/linkedin-dark.svg" class="dark"/>
+    </a>
+  </_Footer>;
 }
